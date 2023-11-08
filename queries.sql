9398f25c857523b14a8e1a78e0b57256fadd5b6a
ProoChuck <rssnow58@gmail.com>
2023-11-07 18:18:21 -0600

    Finished SQL Task 1

--- queries.sql
+++ queries.sql
@@ -1,5 +1,16 @@
 --Part 1
+--CREATE TABLE job (
+--  id INT,
+--  employer VARCHAR(255),
+--  name VARCHAR(255),
+--  skills VARCHAR(255)
+--);
 
+SELECT
+   COLUMN_NAME,
+   DATA_TYPE
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_NAME = 'job';
 --Part 2
 
 --Part 3
