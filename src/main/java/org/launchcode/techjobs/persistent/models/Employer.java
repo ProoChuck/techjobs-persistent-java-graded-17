963193972622b4cbb6cdcbe9ae70f084da8ca797
ProoChuck <rssnow58@gmail.com>
2023-11-09 22:47:30 -0600

    Task 3 completed.  Next Task 4

--- src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
@@ -1,13 +1,26 @@
 package org.launchcode.techjobs.persistent.models;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
+
 @Entity
 public class Employer extends AbstractEntity {
     @Size(min = 1, max = 52, message = "Name must be between 1 and 52 characters")
     @NotBlank(message = "Name cannot be empty")
     private String location;
+    @JoinColumn(name = "employer_id")
+    @OneToMany(mappedBy = "employer")
+    private final List<Job> jobs = new ArrayList<>();
+    public List<Job> getJobs() {
+        return jobs;
+    }
 
     public String getLocation() {
         return location;
