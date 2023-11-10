72ba9306f79f2b5f7acbc8405e4c2823ac2bfc44
ProoChuck <rssnow58@gmail.com>
2023-11-10 12:14:55 -0600

    Task 2 of part 4 completed.  Next Part 3

--- src/main/java/org/launchcode/techjobs/persistent/models/Job.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Job.java
@@ -2,16 +2,26 @@ package org.launchcode.techjobs.persistent.models;
 
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.ManyToOne;
+
+import java.util.List;
 
 @Entity
 public class Job extends AbstractEntity {
     @ManyToOne
     private Employer employer;
-    private String skills;
+    @ManyToMany
+    private List<Skill> skills;
 
 
     public Job() {
+    }
+
+    public Job(Employer employer, List<Skill> skills)
+    {
+        this.employer = employer;
+        this.skills = skills;
     }
 
     // Getters and setters.
@@ -23,11 +33,11 @@ public class Job extends AbstractEntity {
         this.employer = employer;
     }
 
-    public String getSkills() {
+    public List<Skill> getSkills() {
         return skills;
     }
 
-    public void setSkills(String skills) {
+    public void setSkills(List<Skill> skills) {
         this.skills = skills;
     }
 
