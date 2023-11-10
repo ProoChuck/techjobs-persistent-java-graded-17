8947c85d51b81b5cfe653c85c3ad0c31a3a1d4cb
ProoChuck <rssnow58@gmail.com>
2023-11-10 11:41:26 -0600

    Task 4 part 1 completed.  Next to update job model Class to reflect changes made in Skills connecting the two.

--- src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
@@ -1,13 +1,20 @@
 package org.launchcode.techjobs.persistent.models;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToMany;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
+
+import java.util.ArrayList;
+import java.util.List;
+
 @Entity
 public class Skill extends AbstractEntity {
     @Size(min = 1, max = 200, message = "Name must be between 1 and 200 characters")
     @NotBlank(message = "Name cannot be empty")
     private String description;
+    @ManyToMany(mappedBy="skills")
+    private List<Job> jobs = new ArrayList<>();
 
     public String getDescription() {
         return description;
@@ -16,6 +23,15 @@ public class Skill extends AbstractEntity {
     public void setDescription(String description) {
         this.description = description;
     }
+
+    public List<Job> getJobs() {
+        return jobs;
+    }
+
+    public void setJobs(List<Job> jobs) {
+        this.jobs = jobs;
+    }
+
     /*no arg constructor*/
     public Skill(){
     }
